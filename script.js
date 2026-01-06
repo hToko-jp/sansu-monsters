89d4b15bc14f74cc6cf4405e3aa5c18ae1eed7d9
hToko-jp <tokoro.h@geducation.memuro.net>
2026-01-06 14:06:51 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// Firebase Config
-const firebaseConfig = {
-    apiKey: "AIzaSyB-RIVd3JOk6wpI7O90-HuhZIr-vvuxD7w",
-    authDomain: "sansu-monster.firebaseapp.com",
-    projectId: "sansu-monster",
-    storageBucket: "sansu-monster.firebasestorage.app",
-    messagingSenderId: "421981273412",
-    appId: "1:421981273412:web:4594932bf0e8c6f63a2b11",
-    measurementId: "G-L6JD4MKGEX"
-};
-
-// Initialize Firebase (v8 compat)
+// Firebase Initialize (Config is loaded from external firebase-config.js)
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
